4316aba281e42b7ac2df63130c041a86b23ee6a8
anhlehoangg2003@gmail.com <anhlehoangg2003@gmail.com>
2025-07-16 11:17:02 +0700

    add crud feat/blog

--- src/routers/users.routers.ts
+++ src/routers/users.routers.ts
@@ -8,6 +8,7 @@ import AppointmentController from '~/controller/AppointmentControler'
 import StaffController from '~/controller/StaffController'
 import AdminController from '~/controller/AdminController'
 import { PatientController } from '~/controller/PatientController'
+import BlogController from '~/controller/BlogController'
 const router = Router()
 const userController = new UserController()
 const slotController = new SlotController()
@@ -15,6 +16,7 @@ const appointmentController = new AppointmentController()
 const staffController = new StaffController()
 const adminController = new AdminController()
 const patientController = new PatientController()
+const blogController = new BlogController()
 // const appointmentController = new AppointmentController()
 
 router.post('/signup/staff', verifyToken, authorize(['admin']), adminController.signupStaffAccount)
@@ -120,5 +122,11 @@ router.post(
 )
 
 router.get('/getBloodBank', verifyToken, authorize(['staff']), staffController.getBloodBank)
+
+router.get('/blogs', blogController.getBlogs)
+router.get('/blogs/:blogId', blogController.getBlogById)
+router.post('/blogs/create', verifyToken, authorize(['admin']), blogController.createBlog)
+router.put('/blogs/:blogId', verifyToken, authorize(['admin']), blogController.updateBlog)
+router.delete('/blogs/:blogId', verifyToken, authorize(['admin']), blogController.deleteBlog)
 
 export default router
